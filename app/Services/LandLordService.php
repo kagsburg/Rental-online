5702d4a8c3e7891476eea5325148084365d22766
kalemakk <kalemarnld@gmail.com>
2021-09-20 14:51:33 +0300

    intial commit for landlord controller

--- app/Services/LandLordService.php
+++ app/Services/LandLordService.php
@@ -4,7 +4,17 @@
 namespace App\Services;
 
 
+use App\Http\Controllers\LandLordController;
+use App\Http\Resources\LandLordCollection;
+use App\Models\LandLord;
+
 class LandLordService
 {
+    public function getLandLords(){
+//        return LandLord::with('user')->get();
+        return new LandLordCollection(LandLord::all());
+    }
+
+
 
 }
