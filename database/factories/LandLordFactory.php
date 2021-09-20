5e8db318a018ca4b29468b2f53a057c48a67a631
kalemakk <kalemarnld@gmail.com>
2021-09-20 14:36:28 +0300

    added new models

---
+++ database/factories/LandLordFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LandLord;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LandLordFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = LandLord::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/PropertyUnitFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PropertyUnit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PropertyUnitFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PropertyUnit::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
