3afe101aa422992c50139557adc42968a21280a3
Kaganzi Timothy <kagsburg@gmail.com>
2021-12-22 13:14:39 +0300

    added seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Role::create([
+            'role_name' => 'System Admin',
+            'description' =>'this is the administer of the system',
+        ]);
+    }
+}
